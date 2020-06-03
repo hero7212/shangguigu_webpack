8933af99e3b9435c761e6400c633817a6d4f12d3
hero7212 <2423467330@qq.com>
2020-06-03 17:54:45 +0800

    p17

---
+++ 14js压缩/src/js/index.js
@@ -0,0 +1,18 @@
+import '@babel/polyfill'
+
+const add = (x, y) => {
+  return x + y;
+}
+
+
+// eslint-disable-next-line no-console
+console.log(add(2, 5));
+
+const promise = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('定时器执行完了')
+    resolve()
+  }, 1000)
+})
+
+console.log(promise)
