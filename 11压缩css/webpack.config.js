072650988f7435f2aa29bde8d77949e7f94b1b1c
hero7212 <2423467330@qq.com>
2020-06-03 15:15:54 +0800

    p14

---
+++ 11压缩css/webpack.config.js
@@ -0,0 +1,51 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+
+// 设置nodejs环境变量
+// process.env.NODE_ENV = 'development'
+
+// optimize-css-assets-webpack-plugin
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: 'js/built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                // postcss的插件
+                                require('postcss-preset-env')()
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            // 对输出的文件重命名
+            filename: 'css/built.css'
+        }),
+        // 压缩css
+        new OptimizeCssAssetsWebpackPlugin()
+    ],
+    mode: 'development'
+}
